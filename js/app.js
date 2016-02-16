03da3f922139a3b8e1d48f8242f9541c0c989697
Anatol Manoila  <anatol.manoila@tellence.com>
2016-02-16 17:51:16 +0200

    chore(master): clean state

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module( 'weatherApp', ['ngRoute', 'ngAnimate'] );
+var app = angular.module( 'weatherApp', ['ngRoute', 'ngResource', 'ngAnimate'] );
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -19,13 +19,6 @@ app.config(function($routeProvider) {
 //==== SERVICES
 app.service('nameService', function() {
 
-    var that = this;
-    this.name = 'John Doe';
-
-    this.nameLength = function() {
-        return that.name.length;
-    };
-
 });
 
 //==== CONTROLLERS
@@ -37,74 +30,12 @@ app.controller('NavController', ['$scope','$location',function($scope, $location
     };
 }]);
 
-app.controller('MainController', ['$scope', '$filter', '$http', '$log', 'nameService',function( $scope, $filter, $http, $log, nameService) {
-
-    $scope.peopleArray = [
-        {
-            name: 'Jane Maxwell Doe',
-            address: '555 Main St',
-            city: 'New York',
-            state: 'NY',
-            zip: '211222'
-        },
-        {
-            name: 'Frank Sinatra',
-            address: '2311 Wall St',
-            city: 'Miami',
-            state: 'FL',
-            zip: '564222'
-        },
-        {
-            name: 'Jason Statham',
-            address: '77 England St',
-            city: 'London',
-            state: 'UK',
-            zip: '168822'
-        }
-
-    ];
-
-    $scope.formattedAddress = function(person) {
-        return person.address + ',' + person.city + ', ' + person.state;
-    };
-
+app.controller('MainController', ['$scope', '$filter', '$http', '$log', 'nameService',function( $scope, $filter, $http, $log) {
 
 }]);
 
-app.controller('SecondController', ['$scope', '$filter', '$http', '$log', '$routeParams', 'nameService', function( $scope, $filter, $http, $log, $routeParams, nameService) {
+app.controller('SecondController', ['$scope', '$filter', '$http', '$log', '$routeParams', 'nameService', function( $scope, $filter, $http, $log, $routeParams) {
 
-    $scope.name = nameService.name;
-    $scope.num = $routeParams.num || 1;
-
-    $scope.$watch('name', function() {
-        nameService.name = $scope.name;
-    });
 }]);
 
 //==== DIRECTIVES
-
-app.directive('searchResult', function() {
-    //it will return an obj; this object will be my directive
-    return {
-        templateUrl:'views/partials/search-result.html',
-        replace: true,
-        //now this becomes the model of the directive; not the model of the ctrl that the directive sits in the view of; so that you could use the directive on various pages because it has its model;
-        scope: {
-            herPerson: '=', //two-way binding meaning that if I alter this object via the directive its gonna be changed in the model as well
-            formattedAddressFunction: '&' //it's a Fn
-        },
-        transclude: true // i want to do transclusion
-    };
-});
-
-
-
-
-// link: function(scope, elements, attrs) {
-//     console.log('Linking...');
-//     if (scope.herPerson.name === 'Frank Sinatra') {
-//         elements.removeAttr('class');
-//     }
-//     console.log(scope);
-//     console.log(elements);
-// }
